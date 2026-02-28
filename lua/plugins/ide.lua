9b436f4969e07a4ad0c59a675eb712c2393001f9
Shaun Pitchers <shaunpitchers@disroot.org>
2026-02-28 10:18:17 +0000

    Minimal Set Up V0.1

--- lua/plugins/ide.lua
+++ lua/plugins/ide.lua
@@ -1,15 +1,15 @@
 return {
 	-- Function signatures
-	-- {
-	-- 	"ray-x/lsp_signature.nvim",
-	-- 	event = "LspAttach",
-	-- 	config = function()
-	-- 		require("lsp_signature").setup({
-	-- 			bind = true,
-	-- 			hint_enable = false,
-	-- 			floating_window = true,
-	-- 			handler_opts = { border = "rounded" },
-	-- 		})
-	-- 	end,
-	-- },
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "LspAttach",
+		config = function()
+			require("lsp_signature").setup({
+				bind = true,
+				hint_enable = false,
+				floating_window = true,
+				handler_opts = { border = "rounded" },
+			})
+		end,
+	},
 }
